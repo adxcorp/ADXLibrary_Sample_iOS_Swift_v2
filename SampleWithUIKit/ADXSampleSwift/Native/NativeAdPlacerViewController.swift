114aa31dc23c4ceedd563696c0ec7ba684539ee5
JCLEE <jclee@neptunecompany.kr>
2026-04-01 11:15:34 +0900

    [수정] Native AdPlacer 예제 코드 업데이트

--- SampleWithUIKit/ADXSampleSwift/Native/NativeAdPlacerViewController.swift
+++ SampleWithUIKit/ADXSampleSwift/Native/NativeAdPlacerViewController.swift
@@ -16,17 +16,26 @@ class NativeAdPlacerViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        ADXNativeAdFactory.sharedInstance().setRenderingViewClass(ADX_NATIVE_AD_UNIT_ID, 
-                                                                  renderingViewClass: NativeAdView.self)
+        ADXNativeAdFactory.sharedInstance().setRenderingViewClass(
+            ADX_NATIVE_AD_UNIT_ID,
+            renderingViewClass: NativeAdView.self)
         
         let adPositioning = ADXAdPositioning()
         adPositioning.addFixedIndexPath(IndexPath(row: 0, section: 0))
         adPositioning.enableRepeatingPositions(withInterval: 3)
         
-        self.placer = ADXNativeAdFactory.sharedInstance().getCollectionViewAdPlacer(ADX_NATIVE_AD_UNIT_ID, 
-                                                                                    collectionView: collectionView,
-                                                                                    viewController: self,
-                                                                                    adPositioning: adPositioning)
+        let sizeHandler: ADXNativeViewSizeHandler = { maximumWidth in
+            return CGSize(width: maximumWidth, height: 300.0)
+        }
+
+        self.placer = ADXNativeAdFactory.sharedInstance().getCollectionViewAdPlacer(
+            ADX_NATIVE_AD_UNIT_ID,
+            collectionView: collectionView,
+            viewController: self,
+            viewSizeHandler: sizeHandler,
+            adPositioning: adPositioning
+        )
+        
         self.placer.loadAds(forAdUnitID: ADX_NATIVE_AD_UNIT_ID)
     }
 }
